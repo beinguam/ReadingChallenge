797aa0801d1916a74591ad76e11870bcfa5b08df
Bret <bretandkatey@gmail.com>
2019-08-20 11:12:57 -0600

    Stubbed out Organizations controller

---
+++ webApi/ReadingChallengeWebApi/ReadingChallengeWebApi/Controllers/OrganizationsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReadingChallengeWebApi.Models;
+
+namespace ReadingChallengeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationsController : ControllerBase
+    {
+        private readonly ReadingChallengeDBContext _context;
+
+        public OrganizationsController(ReadingChallengeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Organizations
+        [HttpGet]
+        public IEnumerable<Organizations> GetOrganizations()
+        {
+            return _context.Organizations;
+        }
+
+        // GET: api/Organizations/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrganizations([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var organizations = await _context.Organizations.FindAsync(id);
+
+            if (organizations == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(organizations);
+        }
+
+        // PUT: api/Organizations/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrganizations([FromRoute] int id, [FromBody] Organizations organizations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != organizations.OrgId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(organizations).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrganizationsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Organizations
+        [HttpPost]
+        public async Task<IActionResult> PostOrganizations([FromBody] Organizations organizations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Organizations.Add(organizations);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrganizationsExists(organizations.OrgId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOrganizations", new { id = organizations.OrgId }, organizations);
+        }
+
+        // DELETE: api/Organizations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrganizations([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var organizations = await _context.Organizations.FindAsync(id);
+            if (organizations == null)
+            {
+                return NotFound();
+            }
+
+            _context.Organizations.Remove(organizations);
+            await _context.SaveChangesAsync();
+
+            return Ok(organizations);
+        }
+
+        private bool OrganizationsExists(int id)
+        {
+            return _context.Organizations.Any(e => e.OrgId == id);
+        }
+    }
+}
--- webApi/ReadingChallengeWebApi/ReadingChallengeWebApi/Startup.cs
+++ webApi/ReadingChallengeWebApi/ReadingChallengeWebApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+//using EFGetStarted.AspNetCore.ExistingDb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ReadingChallengeWebApi
 {
@@ -25,6 +27,9 @@ namespace ReadingChallengeWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=DESKTOP-79OEU72\SQLEXPRESS;Database=ReadingChallengeDB;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<Models.ReadingChallengeDBContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
