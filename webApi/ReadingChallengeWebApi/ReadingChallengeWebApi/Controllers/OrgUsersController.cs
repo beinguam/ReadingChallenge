20825de5d84785992daa49ef66ca18a6a868a92a
Bret <bretandkatey@gmail.com>
2019-08-21 12:25:23 -0600

    Added remaining controllers

---
+++ webApi/ReadingChallengeWebApi/ReadingChallengeWebApi/Controllers/OrgUsersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReadingChallengeWebApi.Models;
+
+namespace ReadingChallengeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrgUsersController : ControllerBase
+    {
+        private readonly ReadingChallengeDBContext _context;
+
+        public OrgUsersController(ReadingChallengeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrgUsers
+        [HttpGet]
+        public IEnumerable<OrgUsers> GetOrgUsers()
+        {
+            return _context.OrgUsers;
+        }
+
+        // GET: api/OrgUsers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrgUsers([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var orgUsers = await _context.OrgUsers.FindAsync(id);
+
+            if (orgUsers == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orgUsers);
+        }
+
+        // PUT: api/OrgUsers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrgUsers([FromRoute] int id, [FromBody] OrgUsers orgUsers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != orgUsers.OrgUserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orgUsers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrgUsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrgUsers
+        [HttpPost]
+        public async Task<IActionResult> PostOrgUsers([FromBody] OrgUsers orgUsers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.OrgUsers.Add(orgUsers);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrgUsers", new { id = orgUsers.OrgUserId }, orgUsers);
+        }
+
+        // DELETE: api/OrgUsers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrgUsers([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var orgUsers = await _context.OrgUsers.FindAsync(id);
+            if (orgUsers == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrgUsers.Remove(orgUsers);
+            await _context.SaveChangesAsync();
+
+            return Ok(orgUsers);
+        }
+
+        private bool OrgUsersExists(int id)
+        {
+            return _context.OrgUsers.Any(e => e.OrgUserId == id);
+        }
+    }
+}
---
+++ webApi/ReadingChallengeWebApi/ReadingChallengeWebApi/Controllers/UserBooksController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReadingChallengeWebApi.Models;
+
+namespace ReadingChallengeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserBooksController : ControllerBase
+    {
+        private readonly ReadingChallengeDBContext _context;
+
+        public UserBooksController(ReadingChallengeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserBooks
+        [HttpGet]
+        public IEnumerable<UserBooks> GetUserBooks()
+        {
+            return _context.UserBooks;
+        }
+
+        // GET: api/UserBooks/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserBooks([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userBooks = await _context.UserBooks.FindAsync(id);
+
+            if (userBooks == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userBooks);
+        }
+
+        // PUT: api/UserBooks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserBooks([FromRoute] int id, [FromBody] UserBooks userBooks)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userBooks.UserBookId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userBooks).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserBooksExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserBooks
+        [HttpPost]
+        public async Task<IActionResult> PostUserBooks([FromBody] UserBooks userBooks)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.UserBooks.Add(userBooks);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserBooks", new { id = userBooks.UserBookId }, userBooks);
+        }
+
+        // DELETE: api/UserBooks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserBooks([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userBooks = await _context.UserBooks.FindAsync(id);
+            if (userBooks == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserBooks.Remove(userBooks);
+            await _context.SaveChangesAsync();
+
+            return Ok(userBooks);
+        }
+
+        private bool UserBooksExists(int id)
+        {
+            return _context.UserBooks.Any(e => e.UserBookId == id);
+        }
+    }
+}
