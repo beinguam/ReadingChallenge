c97d9e0e35367f66029c57f8be2ae9e28f4fef08
Bret <bretandkatey@gmail.com>
2019-09-03 13:20:43 -0600

    Stubbed out all services

--- readingChallenge/src/app/components/challenges/challenges.component.ts
+++ readingChallenge/src/app/components/challenges/challenges.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChallengesService } from 'src/app/services/challenges.service';
+
 @Component({
   selector: 'app-challenges',
   templateUrl: './challenges.component.html',
   styleUrls: ['./challenges.component.css']
 })
 export class ChallengesComponent implements OnInit {
+  loading = true;
+  results = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private cs: ChallengesService) {
   }
 
+  ngOnInit() {
+    this.cs.getAll().subscribe((data) => {
+      this.loading = false;
+      this.results = data;
+    });
+  }
 }
--- readingChallenge/src/app/components/users/users.component.ts
+++ readingChallenge/src/app/components/users/users.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsersService } from 'src/app/services/users.service';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  loading = true;
+  results = null;
 
-  constructor() { }
+  constructor(private us: UsersService) {
+  }
 
   ngOnInit() {
+    this.us.getAll().subscribe((data) => {
+      this.loading = false;
+      this.results = data;
+    });
   }
 
 }
