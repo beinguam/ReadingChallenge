da090e884ab06fe4537d156dec1c9933331c2193
Bret <bretandkatey@gmail.com>
2019-09-23 11:48:49 -0600

    Add login component to router outlet

--- readingChallenge/src/app/app-routing/app-routing.module.ts
+++ readingChallenge/src/app/app-routing/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdministrationComponent } from '../components/administration/administration.component';
 import { UsersComponent } from '../components/users/users.component';
 import { ChallengesComponent } from '../components/challenges/challenges.component';
+import { LoginComponent } from '../components/login/login.component';
 
 const routes: Routes = [
+        { path: 'login', component: LoginComponent},
         { path: 'admin', component: AdministrationComponent},
         { path: 'users', component: UsersComponent},
         { path: 'challenges', component: ChallengesComponent},
