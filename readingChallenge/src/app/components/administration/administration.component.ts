a1da8747661d22f1ff21cf497453989520ab7e93
Bret Kendall <bret.kendall@kentechservices.com>
2019-10-01 11:31:55 -0600

    Displayed Challenges for Organization selected

--- readingChallenge/src/app/components/administration/administration.component.ts
+++ readingChallenge/src/app/components/administration/administration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { OrganizationsService } from '../../services/organizations.service';
 import { Organizations } from '../../models/organizations-model';
+import { ChallengesService } from 'src/app/services/challenges.service';
+import { Challenges } from 'src/app/models/challenges-model';
 
 @Component({
   selector: 'app-administration',
@@ -11,15 +13,20 @@ import { Organizations } from '../../models/organizations-model';
 export class AdministrationComponent implements OnInit {
   results: Organizations[];
   userId = 1;
+  chalResults: Challenges[];
 
-  constructor(private os: OrganizationsService) {
+  constructor(private os: OrganizationsService, private c: ChallengesService) {
   }
 
   ngOnInit() {
     this.os.getOrganization(this.userId).subscribe((data) => {
-      this.results = data;
-      console.log(this.results);
+      this.results = data;      
     });
   }
 
+  choseOrganization() {
+    this.c.getAll().subscribe((data) => {
+      this.chalResults = data;      
+    });
+  }
 }
